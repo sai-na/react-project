dbb23bfa3a2d70f2b765bbaacae016ee22d231cc
sainath378 <saipkmm100@gmail.com>
2022-01-06 21:53:41 +0530

    update

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,31 @@
 // https://reactjs.org/docs/hooks-state.html
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA"); //restructuring
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
+    const [pets, setPets] = useState([]);
     const breeds = [];
+
+    useEffect(() => {
+        requestPets();
+    }, []);
+
+
+    async function requestPets() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=
+        ${location}&breed=${breed}`
+        );
+
+        const json = await res.json();
+
+        setPets(json.pets);
+
+    }
 
     return (
 
@@ -69,6 +88,13 @@ const SearchParams = () => {
 
                 <button>Submit</button>
             </form>
+
+            {
+                pets.map((pet) => (
+                    <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id}
+                    />
+                ))
+            }
 
         </div>
     );
