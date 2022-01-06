2d69c7efe319c2bda9c51e244b70dfb295a7696b
sainath378 <saipkmm100@gmail.com>
2022-01-06 13:29:59 +0530

    Update SearchParams.js

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,7 @@
 // https://reactjs.org/docs/hooks-state.html
 import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabtbit", "reptile"];
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA"); //react restructuring
     return (
@@ -9,7 +11,20 @@ const SearchParams = () => {
                 <label htmlFor="location"> Location
                     <input id="location" onChange={(e) => setLocation(e.target.value)}
 
-                        value={location} placeholder="Location" />
+                        value={location} placeholder="Location"
+                    />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value="animal"
+                        onChange={(e) => setAnimal(e.target.value)}
+                        onBlur={e.}
+                    >
+
+                    </select>
+
                 </label>
                 <button>Submit</button>
             </form>
