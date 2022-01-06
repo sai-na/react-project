eb8c62374e256988b19164849d1ba74be594fbca
sainath378 <saipkmm100@gmail.com>
2022-01-06 15:16:40 +0530

    Hooks animals and breeds

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,13 @@
 // https://reactjs.org/docs/hooks-state.html
 import { useState } from "react";
 
-const ANIMALS = ["bird", "cat", "dog", "rabtbit", "reptile"];
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seattle, WA"); //react restructuring
+    const [location, setLocation] = useState("Seattle, WA"); //restructuring
+    const [animal, setAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+    const breeds = [];
+
     return (
 
         <div className="search-params">
@@ -18,14 +22,51 @@ const SearchParams = () => {
                     Animal
                     <select
                         id="animal"
-                        value="animal"
+                        value={animal}
                         onChange={(e) => setAnimal(e.target.value)}
-                        onBlur={e.}
+                        onBlur={(e) => setAnimal(e.target.value)}
                     >
+                        <option></option>
+                        {
+                            ANIMALS.map(animal => (
+                                <option value={animal} key={animal}>
+                                    {animal}
+                                </option>
+
+
+                            ))
+                        }
+
 
                     </select>
 
                 </label>
+
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        id="breed"
+                        value={breed}
+                        onChange={(e) => setBreed(e.target.value)}
+                        onBlur={(e) => setBreed(e.target.value)}
+                    >
+                        <option></option>
+                        {
+                            breeds.map(breed => (
+                                <option value={breed} key={breed}>
+                                    {breed}
+                                </option>
+
+
+                            ))
+                        }
+
+
+                    </select>
+
+                </label>
+
+
                 <button>Submit</button>
             </form>
 
