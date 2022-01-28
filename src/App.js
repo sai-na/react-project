476b52827b687cfedcb64926e263ca95ce9ab0f6
sainath378 <92171156+sainath378@users.noreply.github.com>
2022-01-28 08:04:44 +0530

    my

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 
 import SearchParams from './SearchParams';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { StrictMode } from 'react';
+import Details from './Details';
 
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+
+          <Route path="/">
+
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
 
 
-    </div>
+    </div >
   )
 }
 
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+    return <h2>Hi lololol omg wtf!</h2>;
+};
+
+export default Details;
