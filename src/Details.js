079cad5f870cce3ea195c95078eeb6e79a1f7798
sainath378 <92171156+sainath378@users.noreply.github.com>
2022-01-30 15:41:08 +0530

    Update Details.js

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,41 @@
-const Details = () => {
-    return <h2>Hi lololol omg wtf!</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+class Details extends Component {
+    constructor() {
+        super();
+        this.state = { loading: true };
+    }
+
+    async componentDidMount() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        );
+        const json = await res.json();
+        this.setState(
+            Object.assign(
+                {
+                    loading: false
+
+                },
+                json.pets[0]
+
+            )
+        );
+    }
+    render() {
+        const { animal, breed, city, state, description, name } = this.state;
+        console.log(this.state);
+        return (
+            <div className="details">
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                <button>{`Adopt ${name} `}</button>
+                <p>{description}</p>
+            </div>
+        );
+    }
+
+
+}
+export default withRouter(Details);
